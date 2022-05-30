98ec3084ce9967e2be24302f66cf0e073be7068f
MagicMoleMM <101579330+MagicMoleMM@users.noreply.github.com>
2022-05-30 19:34:48 +0300

    30.05.2022 19:30

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,18 @@
-const menuLoaded = (newMenu) => {
+import { createAction } from '@reduxjs/toolkit'
+
+const menuLoaded = createAction('MENU_LOADED');
+const menuRequested = createAction('MENU_REQUESTED');
+// const addedToCart = createAction('ITEM_ADD_TO_CART');
+
+const addedToCart = (id) => {
     return {
-        type: 'MENU_LOADED',
-        payload: newMenu
+        type: 'ITEM_ADD_TO_CART',
+        payload: id
     }
-}
+};
 
 export {
-    menuLoaded
+    menuLoaded,
+    menuRequested,
+    addedToCart
 };
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import {MainPage, CartPage} from '../pages';
-import WithRestoService from '../hoc';
 import AppHeader from '../app-header';
 import Background from './food-bg.jpg';
 
-const App = ({RestoService}) => {
-    console.log(RestoService.getMenuItems())
+const App = () => {
+
     return (
         <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
             <AppHeader total={50}/>
@@ -15,4 +14,4 @@ const App = ({RestoService}) => {
     )
 }
 
-export default WithRestoService()(App);
+export default App;
